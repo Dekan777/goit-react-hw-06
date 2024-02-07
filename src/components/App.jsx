aa45590428dd0c85df050b437ba965655888debe
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-02-07 22:56:14 +0100

    w

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,14 @@
-import './App.css';
+import { Layout } from './Layout/Layout';
+import { AppBar } from './AppBar/AppBar';
+import { TaskForm } from './TaskForm/TaskForm';
+import { TaskList } from './TaskList/TaskList';
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
+  );
 };
