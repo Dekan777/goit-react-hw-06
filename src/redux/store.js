6850d1ef720d743a8298e21f04662c55d9bf625a
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-02-07 23:17:30 +0100

    r

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,1 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { tasksReducer } from './tasksSlice';
-import { filtersReducer } from './filtersSlice';
-
-export const store = configureStore({
-  reducer: {
-    tasks: tasksReducer,
-    filters: filtersReducer,
-  },
-});
+// import { createStore } from "redux";
