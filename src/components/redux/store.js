74ab47ed287ab2b1bc51f91c6ed1ae17b6b7f4a3
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-02-08 11:56:47 +0100

    E

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,2 +1,9 @@
-// import { createStore } from "redux";
+import { configureStore } from '@reduxjs/toolkit';
 
+
+export const store = configureStore({
+    reducer: {
+        contacs: ,
+        filters: ,
+    },
+});
